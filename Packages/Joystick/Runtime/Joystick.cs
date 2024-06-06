54a586ef9828065951c844cd1a5b8618e52bfaa5
Pleija 永恒之恋 <cloud@shopes.cn>
2024-06-06 09:56:57 +0800

    fix joystick moving close #313 6.6.9[16.63] 第-30.4周 小满/芒种11-0

--- Packages/Joystick/Runtime/Joystick.cs
+++ Packages/Joystick/Runtime/Joystick.cs
@@ -12,24 +12,34 @@ namespace zFrame.UI
     public class Joystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
     {
         public static Joystick instance;
-        public float maxRadius = 38; //摇杆移动最大半径
+        public float maxRadius = 38;                     //摇杆移动最大半径
         public Direction activatedAxis = Direction.Both; //选择激活的轴向
-        [SerializeField] bool dynamic = true; // 是否为动态摇杆
-        [SerializeField] bool showDirectionArrow = true; // 是否展示指向器
+
+        [SerializeField]
+        bool dynamic = true; // 是否为动态摇杆
+
+        [SerializeField]
+        bool showDirectionArrow = true; // 是否展示指向器
+
         [Space(8)]
         public JoystickEvent OnValueChanged = new JoystickEvent(); //事件 ： 摇杆被 拖拽时
 
         public UnityEvent OnDragging;
 
-        #region Property
-        public bool IsDraging { get { return fingerId != int.MinValue && isMoving; } } //摇杆拖拽状态
-        public bool ShowDirectionArrow { get => showDirectionArrow; set => showDirectionArrow = value; }  // 是否展示指向器
+#region Property
+        public bool IsDraging {
+            get { return fingerId != int.MinValue && isMoving; }
+        } //摇杆拖拽状态
+
+        public bool ShowDirectionArrow {
+            get => showDirectionArrow;
+            set => showDirectionArrow = value;
+        } // 是否展示指向器
+
         public bool IsDynamic //运行时代码配置摇杆是否为动态摇杆
         {
-            set
-            {
-                if (dynamic != value)
-                {
+            set {
+                if (dynamic != value) {
                     dynamic = value;
                     ConfigJoystick();
                 }
@@ -38,9 +48,9 @@ namespace zFrame.UI
         }
 
         public bool isMoving;
-        #endregion
+#endregion
 
-        #region MonoBehaviour functions
+#region MonoBehaviour functions
         private void Awake()
         {
             instance = this;
@@ -48,17 +58,20 @@ namespace zFrame.UI
 
         void Start()
         {
-
             backGroundOriginLocalPostion = backGround.localPosition;
         }
 
-        void Update() => OnValueChanged.Invoke(knob.localPosition / maxRadius); //fixedupdate 为物理更新，摇杆操作放在常规 update 就好
-        void OnDisable() => RestJoystick(); //意外被 Disable 各单位需要被重置
-        void OnValidate() => ConfigJoystick(); //Inspector 发生改变，各单位需要重新配置，编辑器有效
-        #endregion
+        void Update() =>
+            OnValueChanged.Invoke(knob.localPosition
+                / maxRadius); //fixedupdate 为物理更新，摇杆操作放在常规 update 就好
 
-        #region The implement of pointer event Interface
+        void OnDisable() => RestJoystick();    //意外被 Disable 各单位需要被重置
+        void OnValidate() => ConfigJoystick(); //Inspector 发生改变，各单位需要重新配置，编辑器有效
+#endregion
+
+#region The implement of pointer event Interface
         private bool beginDrag;
+
         void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
         {
             if (eventData.pointerId < -1 || IsDraging) return; //适配 Touch：只响应一个Touch；适配鼠标：只响应左键
@@ -74,13 +87,11 @@ namespace zFrame.UI
             OnPointerDown.Invoke(eventData.position);
         }
 
-
         void IDragHandler.OnDrag(PointerEventData eventData)
         {
             if (fingerId != eventData.pointerId) return;
 
             if (beginDrag) {
-                isMoving = false;
                 beginDrag = false;
                 //Debug.Log("drag");
                 pointerDownPosition = eventData.position;
@@ -95,38 +106,38 @@ namespace zFrame.UI
                     ;
                 }
             }
-            else {
-                isMoving = true;
-            }
-
-
-            Vector2 direction = eventData.position - (Vector2)pointerDownPosition; //得到BackGround 指向 Handle 的向量
-            float radius = Mathf.Clamp(Vector3.Magnitude(direction), 0, maxRadius); //获取并锁定向量的长度 以控制 Handle 半径
-            Vector2 localPosition = new Vector2()
-            {
-                x = (activatedAxis == Direction.Both || activatedAxis == Direction.Horizontal) ? (direction.normalized * radius).x : 0, //确认是否激活水平轴向
-                y = (activatedAxis == Direction.Both || activatedAxis == Direction.Vertical) ? (direction.normalized * radius).y : 0       //确认是否激活垂直轴向，激活就搞事情
+            isMoving = true;
+            Vector2 direction =
+                eventData.position - (Vector2)pointerDownPosition; //得到BackGround 指向 Handle 的向量
+            float radius =
+                Mathf.Clamp(Vector3.Magnitude(direction), 0, maxRadius); //获取并锁定向量的长度 以控制 Handle 半径
+            Vector2 localPosition = new Vector2() {
+                x = (activatedAxis == Direction.Both || activatedAxis == Direction.Horizontal)
+                    ? (direction.normalized * radius).x : 0, //确认是否激活水平轴向
+                y = (activatedAxis == Direction.Both || activatedAxis == Direction.Vertical)
+                    ? (direction.normalized * radius).y : 0 //确认是否激活垂直轴向，激活就搞事情
             };
-            knob.localPosition = localPosition;      //更新 Handle 位置
-            if (showDirectionArrow)
-            {
+            knob.localPosition = localPosition; //更新 Handle 位置
+
+            if (showDirectionArrow) {
                 if (!arrow.gameObject.activeInHierarchy) arrow.gameObject.SetActive(true);
-                arrow.localEulerAngles = new Vector3(0, 0, Vector2.SignedAngle(Vector2.right, localPosition));
+                arrow.localEulerAngles =
+                    new Vector3(0, 0, Vector2.SignedAngle(Vector2.right, localPosition));
             }
             OnDragging.Invoke();
         }
 
         void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
         {
-            if (fingerId != eventData.pointerId) return;//正确的手指抬起时才会重置摇杆；
+            if (fingerId != eventData.pointerId) return; //正确的手指抬起时才会重置摇杆；
             isMoving = false;
             RestJoystick();
             OnPointerUp.Invoke(eventData.position);
         }
-        #endregion
+#endregion
 
-        #region Assistant functions / fields / structures
-        void RestJoystick()  //重置摇杆数据
+#region Assistant functions / fields / structures
+        void RestJoystick() //重置摇杆数据
         {
             backGround.localPosition = backGroundOriginLocalPostion;
             knob.localPosition = Vector3.zero;
@@ -141,20 +152,28 @@ namespace zFrame.UI
             // handle.GetComponent<Image>().raycastTarget = !dynamic;
         }
 
-        [HideInInspector] public JoystickEvent OnPointerDown = new JoystickEvent(); // 事件： 摇杆被按下时
-        [HideInInspector] public JoystickEvent OnPointerUp = new JoystickEvent(); //事件 ： 摇杆上抬起时
-        [SerializeField, HideInInspector] public Transform knob; //摇杆
-        [SerializeField, HideInInspector] public Transform backGround; //背景
-        [SerializeField, HideInInspector] public Transform arrow; //指向器
+        [HideInInspector]
+        public JoystickEvent OnPointerDown = new JoystickEvent(); // 事件： 摇杆被按下时
+
+        [HideInInspector]
+        public JoystickEvent OnPointerUp = new JoystickEvent(); //事件 ： 摇杆上抬起时
+
+        [SerializeField, HideInInspector]
+        public Transform knob; //摇杆
+
+        [SerializeField, HideInInspector]
+        public Transform backGround; //背景
+
+        [SerializeField, HideInInspector]
+        public Transform arrow; //指向器
+
         private Vector3 backGroundOriginLocalPostion, pointerDownPosition;
         private int fingerId = int.MinValue; //当前触发摇杆的 pointerId ，预设一个永远无法企及的值
-        [System.Serializable] public class JoystickEvent : UnityEvent<Vector2> { }
-        public enum Direction
-        {
-            Both,
-            Horizontal,
-            Vertical
-        }
-        #endregion
+
+        [System.Serializable]
+        public class JoystickEvent : UnityEvent<Vector2> { }
+
+        public enum Direction { Both, Horizontal, Vertical }
+#endregion
     }
 }
